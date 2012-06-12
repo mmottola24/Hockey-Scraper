dd3b09435f398708ab76291eb0ad118ad5659bc6
Michael Mottola <mikemottola@gmail.com>
2012-06-12 01:41:54 -0400

    added function to parse options

--- bin/bhi-api.js
+++ bin/bhi-api.js
@@ -18,26 +18,61 @@ var apiLib = new bhiAPI();
 var command = args.shift();
 
 switch (command) {
+    
+    case 'create' :
+    case 'c':
+        var argument = args.shift();
+        var options = parseOptions(args);
+        
+        // call library to performs task
+        apiLib.create(argument, options);
+        break;
+    
     case '-h' :
     case '--help' :
     case 'help' :
         binHelper.displayHelp();
         break;
+    
     case '-v' :
     case '--version':
     case 'version' :
         binHelper.displayVersion(apiLib.version);
         break;
+    
     default :
         binHelper.displayUsage();
         break;
 }
 
-// loop through args until there is none left
-while (arg = args.shift()) {
-    console.log(arg);
-    
-    
+function parseOptions() {
+    if (args.length > 0) {
+        var options = {};
+        
+        // loop through args until there is none left
+        while ((arg = args.shift())) {
+            
+            // if first char is a dash than option is valid
+            if (arg.charAt(0) == '-') {
+                var option = arg.replace('-','');
+                var value = args.shift();
+                
+                switch (option) {
+                    case 'l' :
+                        options['leage_id'] = value;
+                        break;
+                    case 't' :
+                        options['team_id'] = value;
+                        break;
+                }
+            }          
+        }
+        
+        return options;
+    } else {
+        return {};
+    }
 }
+
 
 
