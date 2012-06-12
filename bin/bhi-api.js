802702fd42adee3f2e1ce370ea250a34e1e90958
Michael Mottola <mikemottola@gmail.com>
2012-06-12 00:01:24 -0400

    restructured app

--- bin/bhi-api.js
+++ bin/bhi-api.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+var bhiAPI = require('../lib/bhi-api');
+
+// get arguments from command
+var args = process.argv.slice(2);
+
+console.log(args);
+
+if (args.length < 2) {
+    console.log('usage: bhi-api <command> (args)');
+    process.exit(0);
+}
+
+//initialize bhiAPI lib
+api = new bhiAPI();
