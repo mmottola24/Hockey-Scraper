af0b3c568383ac0d0832be7ec7db7dea7e18c3c5
Michael Mottola <mikemottola@gmail.com>
2012-06-12 01:42:10 -0400

    Updated help outputc

--- bin/bin-helper.js
+++ bin/bin-helper.js
@@ -3,29 +3,32 @@ BinHelper =  {
    // display the help screen
    displayHelp:  function() {
       var output = [
-        "usage: bhi-api <command> [options]",
+        "usage: bhi-api <command> <argument> [options[]]",
         "",
         "commands:",
         "  create, c, --create         Series of create functions",
-        "  options:",
-        "    -",
+        "  arguments:",
+        "    schedule                  Generates schedule.",
+        "    options:",
+        "      -l, --league            League Id",
+        "      -t, --team              Team Id",
         "",
         "  version, -v, --version      Displays the current version number",
         "  help, -h, --help            Displays the help your looking at"
       ].join('\n')
       
       console.log(output);
-      process.exit(1);
+      process.exit(0);
    },
    
    displayVersion: function(version) {
       console.log('version: ' + version);
-      process.exit(1);
+      process.exit(0);
    },
    
    // display usage message
    displayUsage: function() {
-      console.log('usage: bhi-api <command> (args).  bhi-api help for more info');
+      console.log('usage: bhi-api <command> [argument] [options[]].  bhi-api help for more info');
       process.exit(0);
    }
 };
